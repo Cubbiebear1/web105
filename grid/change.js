e66b451f0161a636683c01f8bdf0ee0f2931ed8d
Becky Johnson <142274651+Cubbiebear1@users.noreply.github.com>
2023-11-02 08:10:43 -0500

    Add files via upload

---
+++ grid/change.js
@@ -0,0 +1,19 @@
+function change01() {
+    document.getElementById("largeImage").src = "../grid/images/thesun-copy.jpg";
+}
+
+function change02() {
+    document.getElementById("largeImage").src = "../grid/images/binky-gimp.jpg";
+}
+
+function change03() {
+    document.getElementById("largeImage").src = "../grid/images/Binkytree.jpg";
+}
+
+function change04() {
+    document.getElementById("largeImage").src = "../grid/images/George.jpg";
+}
+
+function change05() {
+    document.getElementById("largeImage").src = "../grid/images/morning-gimp.jpg";
+}
